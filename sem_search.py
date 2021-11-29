b94173bf8cf9d55bf38827970dd14788f9790e2b
nschantz21 <nschantz21@gmail.com>
2021-11-29 03:40:34 +0000

    updated some of the query builder

--- sem_search.py
+++ sem_search.py
@@ -8,11 +8,15 @@ import os
 from google.cloud import bigquery
 from google.oauth2 import service_account
 import json
+import sentence_transformers
 
 # constants
 AUTH_KEY = json.loads(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-credentials = service_account.Credentials.from_service_account_info(
-    AUTH_KEY)
+credentials = service_account.Credentials.from_service_account_info(AUTH_KEY)
+
+TWITTER_DB_NAME = "nwo-sample.graph.tweets"
+REDDIT_DB_NAME = "nwo-sample.graph.reddit"
+
 
 # functions
 def semantic_search():
@@ -25,15 +29,9 @@ def semantic_search():
 client = bigquery.Client(credentials=credentials)
 
 # Perform a query.
-QUERY = (
-    'SELECT name FROM `nwo-sample.graph.tweets` '
-    'LIMIT 10')
+QUERY = ('SELECT * FROM `{}` LIMIT 10'.format(REDDIT_DB_NAME))
 query_job = client.query(QUERY)  # API request
 rows = query_job.result()  # Waits for query to finish
 
 for row in rows:
-    print(row.name)
-
-
-
-
+    print(row)
